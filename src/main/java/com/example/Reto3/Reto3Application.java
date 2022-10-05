81cb06631bd1a0b8c206d295fb05b0c3ac7699d1
Hollmanick <113219060+Hollmanick@users.noreply.github.com>
2022-10-05 16:35:08 -0500

    “Creacion_CERS”

--- src/main/java/com/example/Reto3/Reto3Application.java
+++ src/main/java/com/example/Reto3/Reto3Application.java
@@ -1,13 +1,13 @@
-package com.example.Reto3;
+package com.example.retos345;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Reto3Application {
+public class Retos345Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Reto3Application.class, args);
+		SpringApplication.run(Retos345Application.class, args);
 	}
 
 }
--- src/test/java/com/example/Reto3/Reto3ApplicationTests.java
+++ src/test/java/com/example/Reto3/Reto3ApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.Reto3;
+package com.example.retos345;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Reto3ApplicationTests {
+class Retos345ApplicationTests {
 
 	@Test
 	void contextLoads() {
