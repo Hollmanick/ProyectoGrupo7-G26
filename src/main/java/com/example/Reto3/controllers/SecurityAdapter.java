59601400a205dc942693de76988823d84a6074da
Hollmanick <113219060+Hollmanick@users.noreply.github.com>
2022-10-07 23:19:08 -0500

    CorreccionSecurityAdapter

--- src/main/java/com/example/Reto3/controllers/SecurityAdapter.java
+++ src/main/java/com/example/Reto3/controllers/SecurityAdapter.java
@@ -2,7 +2,7 @@ package com.example.Reto3.controllers;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,9 +11,7 @@ public class SecurityAdapter extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests(
-            a -> a.antMatchers(
-                ...antPatterns:"/", "/error", "/webjars/**",
-                 "/api/**"
+            a -> a.antMatchers("/", "/error", "/webjars/**","/api/**"
             ).permitAll().anyRequest().authenticated()
         ).exceptionHandling(
             e -> e.authenticationEntryPoint( new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
