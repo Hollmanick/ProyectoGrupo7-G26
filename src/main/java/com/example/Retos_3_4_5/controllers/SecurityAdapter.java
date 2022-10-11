f8ea78e247df6d5934e2eed12c3937ea09d9cbf1
Hollmanick <113219060+Hollmanick@users.noreply.github.com>
2022-10-10 23:42:39 -0500

    CorreccionSecurityAdapter

--- src/main/java/com/example/Retos_3_4_5/controllers/SecurityAdapter.java
+++ src/main/java/com/example/Retos_3_4_5/controllers/SecurityAdapter.java
@@ -11,7 +11,8 @@ public class SecurityAdapter extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests(
-            a -> a.antMatchers("/", "/error", "/webjars/**","/api/**"
+            a -> a.antMatchers(
+                "/", "/error", "/webjars/**","/api/**"
             ).permitAll().anyRequest().authenticated()
         ).exceptionHandling(
             e -> e.authenticationEntryPoint( new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
@@ -19,4 +20,5 @@ public class SecurityAdapter extends WebSecurityConfigurerAdapter {
 
         http.cors().and().csrf().disable();
     }
+    
 }
